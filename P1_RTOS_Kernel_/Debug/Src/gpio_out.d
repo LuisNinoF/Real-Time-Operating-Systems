3bb6881095b73e6a43887d059759f41d7889c7cb
LuisNinoForero <luis.nino0@hm.edu>
2025-11-15 22:39:40 +0100

    update P1_RTOS_Kernel_

--- P1_RTOS_Kernel_/Debug/Src/gpio_out.d
+++ P1_RTOS_Kernel_/Debug/Src/gpio_out.d
@@ -1,18 +1,18 @@
 Src/gpio_out.o: ../Src/gpio_out.c ../Inc/gpio_out.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/core_cm4.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_version.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_compiler.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/mpu_armv7.h \
- C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
 ../Inc/gpio_out.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/core_cm4.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_version.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_compiler.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/cmsis_gcc.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include/mpu_armv7.h:
-C:/Users/lfnin/OneDrive/Documents/Studies/Embedded\ Systems/8.\ Embedded_Expert_IO/5.\ Phase1/6.\ Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
--- P1_RTOS_Kernel_/Debug/Src/subdir.mk
+++ P1_RTOS_Kernel_/Debug/Src/subdir.mk
@@ -41,7 +41,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DNUCLEO_F401RE -DSTM32 -DSTM32F401RETx -DSTM32F4 -DSTM32F401xC -c -I../Inc -I"C:/Users/lfnin/OneDrive/Documents/Studies/Embedded Systems/8. Embedded_Expert_IO/5. Phase1/6. Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/lfnin/OneDrive/Documents/Studies/Embedded Systems/8. Embedded_Expert_IO/5. Phase1/6. Milestones/P2_RTOS_Kernel/F4_chip_headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DNUCLEO_F401RE -DSTM32 -DSTM32F401RETx -DSTM32F4 -DSTM32F401xC -c -I../Inc -I"C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/lfnin/git/Real-Time-Operating-Systems/P1_RTOS_Kernel_/F4_chip_headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/%.o: ../Src/%.s Src/subdir.mk
 	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
